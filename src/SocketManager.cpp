4ebbd8ba1c022ee1f994daed0a1f9baf63931488
MeshCube <ildarc4S@gmail.com>
2024-10-10 21:19:50 +0300

    SocketManager-eng

--- src/SocketManager.cpp
+++ src/SocketManager.cpp
@@ -9,7 +9,7 @@ void SocketManager::createConnectorAddress(struct sockaddr_in& address, int serv
 int SocketManager::bindSocket(int server_fd, const sockaddr_in& address) {
     int result = bind(server_fd, (struct sockaddr*)&address, sizeof(address));
     if (result < 0) {
-        std::cerr << "Ошибка при привязке сокета: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error binding the socket:" << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -17,7 +17,7 @@ int SocketManager::bindSocket(int server_fd, const sockaddr_in& address) {
 int SocketManager::listenSocket(int server_fd, int count) {
     int result = listen(server_fd, count);
     if (result < 0) {
-        std::cerr << "Ошибка при прослушивании сокета: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error listening to the socket: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -26,7 +26,7 @@ int SocketManager::sendMessage(int server_fd, char* message, size_t len) {
     message[len] = '\0';
     int result = send(server_fd, message, len, 0);
     if (result == -1) {
-        std::cerr << "Ошибка отправки сообщения: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error sending the message: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -38,7 +38,7 @@ int SocketManager::recvMessage(int server_fd, char* buffer, int buffer_size) {
 int SocketManager::creatteSocket() {
     int result = socket(AF_INET, SOCK_STREAM, 0);
     if (result == -1) {
-        std::cerr << "Ошибка при создании сокета: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error creating the socket: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -51,7 +51,7 @@ int SocketManager::connectServer(int server_fd, std::string& server_ip, int serv
 
     int result = connect(server_fd, (struct sockaddr*)&server_addres, sizeof(server_addres));
     if (result == -1) {
-        std::cerr << "Ошибка при подключении серверу: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error connecting to the server: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -61,7 +61,7 @@ int SocketManager::acceptConnection(int server_fd, struct sockaddr_in& address) 
 
     int result = accept(server_fd, (struct sockaddr*)&address, &addrlen);
     if (result == -1) {
-        std::cerr << "Ошибка при принятии клиента: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error in accepting a client: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -73,7 +73,7 @@ int SocketManager::addSocketToEpoll(int socket_fd, int epoll_fd) {
 
     int result = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event);
     if (result == -1) {
-        std::cerr << "Ошибка при добавлении сокета в epoll: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error adding socket to epoll: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -81,7 +81,7 @@ int SocketManager::addSocketToEpoll(int socket_fd, int epoll_fd) {
 int SocketManager::epollCreate() {
     int result = epoll_create1(0);
     if (result == -1) {
-        std::cerr << "Ошибка при создании epoll: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error creating the epoll: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
@@ -89,7 +89,7 @@ int SocketManager::epollCreate() {
 int SocketManager::epollWait(int epoll_fd, std::vector<epoll_event>& events, int max_clients) {
     int result = epoll_wait(epoll_fd, events.data(), max_clients, -1);
     if (result == -1) {
-        std::cerr << "Ошибка epoll_wait: " << std::strerror(errno) << std::endl;
+        std::cerr << "Error epoll_wait: " << std::strerror(errno) << std::endl;
     }
     return result;
 }
